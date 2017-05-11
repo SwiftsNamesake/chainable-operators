9268af6a825337ee05cda1fe02934fc3b25bf331
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2017-05-11 20:59:38 +0200

    Removed app/

--- src/Chainable.hs
+++ src/Chainable.hs
@@ -1,14 +1,14 @@
 -- |
 -- Module      : Chainable
 -- Description : Operators are meant to be chained
--- Copyright   : (c) Jonatan H Sundqvist, 2017
+-- Copyright   : (c) Jonatan Sundqvist, 2017
 -- License     : MIT
 -- Maintainer  : Jonatan H Sundqvist
 -- Stability   : experimental
 -- Portability : portable
 --
 
--- TODO | -
+-- TODO | - Shadow Prelude's (>) and (<) (?)
 --        -
 
 -- SPEC | -
@@ -26,31 +26,6 @@ module Chainable where
 
 -- Definitions -------------------------------------------------------------------------------------------------------------------------------------
 
--- |
-class VaryingArg a where {}
-
-
--- |
-class VaryingFold a b c where
-  fold :: (VaryingArg a, VaryingFold a b c) => (b -> a -> b) -> b -> c
-
-
--- |
-instance (VaryingArg a, VaryingFold a b c) => VaryingFold a b ((->) a c) where
-  fold f b a = fold f (f b a)
-
-
--- |
-instance VaryingFold a b b where
-  fold f acc = acc
-
-
--- |
-instance VaryingArg Int
-instance VaryingArg [Char]
-
-
--- |
 -- TODO | - This class should be as flexible as possible
 class Chainable a b c | b -> a where
   chainable :: (a -> a -> Bool) -> a -> b -> c
@@ -77,6 +52,7 @@ instance Chainable a (a, Bool) Bool where
 (=.) :: (Chainable a b c, Eq a) => a -> b -> c
 (=.) = chainable (==)
 
+
 -- | Chainable less-than
 (<.) :: (Chainable a b c, Ord a) => a -> b -> c
 (<.) = chainable (<)
@@ -97,6 +73,7 @@ instance Chainable a (a, Bool) Bool where
 (≥) = chainable (>=)
 
 
+-- |
 -- TODO | - Decide affinity
 infixr 4 =.
 infixr 4 <.
@@ -109,6 +86,8 @@ infixr 4 ≥
 -- |
 main :: IO ()
 main = do
+  -- x₀ = 2.6
+  -- x₁ = n
   putStrLn "Let's see if it works"
-  print $ (fold (++) "Hello" " " "World" :: String)
+  putStrLn $ fold (++) "Hello" " " "World"
   print $ ((0 :: Int) <. ((5 :: Int) <. (10 :: Int) :: (Int, Bool)) :: Bool)
